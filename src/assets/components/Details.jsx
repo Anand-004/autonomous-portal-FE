14f18a199b27c9ba76a23aee80b7c59722ce3264
Anand-004 <anandsince2004@gmail.com>
2024-11-11 20:43:51 +0530

    changed color

--- src/assets/components/Details.jsx
+++ src/assets/components/Details.jsx
@@ -11,8 +11,8 @@ import DescriptionIcon from '@mui/icons-material/Description';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.white,
-    color: theme.palette.common.black,
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
   },
   [`&.${tableCellClasses.body}`]: {
     fontSize: 14,
